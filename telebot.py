7ade5916023905237a719ed78c5e5fc6552a0df7
bloodstalker <thabogre@gmail.com>
2018-04-20 15:12:26 +0430

    update

--- telebot.py
+++ telebot.py
@@ -8,8 +8,14 @@ import sys
 from telegram.ext import Updater, CommandHandler
 import logging
 import imp
+import json
+sys.path.insert(0, "./python")
+from deviutil import Demon_Father
 hived = imp.load_source("hived", "./hived")
 from hived import networth, highpercentdump_bot
+sys.path.insert(0, "/home/bloodstalker/extra/kaminokumo")
+kaminokumo = imp.load_source("kaminokumo", "/home/bloodstalker/extra/kaminokumo/kaminokumo")
+from kaminokumo import mrg
 import json
 
 telekey_json = "./telekey.json"
@@ -21,7 +27,7 @@ def SigHandler_SIGINT(signum, frame):
     sys.exit(0)
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Hi... I'm bloodstalkers bot, nice to make your acquaintance.")
+    bot.send_message(chat_id=update.message.chat_id, text="Hi... I'm devi's bot, nice to make your acquaintance.")
 
 def net(bot, update):
     total = networth()
@@ -32,6 +38,14 @@ def high(bot, update):
     total_str = "".join(total)
     bot.send_message(chat_id=update.message.chat_id, text=total_str)
 
+def mg(bot, update):
+    url = json.load(open("/home/bloodstalker/extra/kaminokumo/data.json"))
+    res = mrg(url["url"])
+    text_str = str()
+    if res: text_str = "marge is up"
+    else: text_str = "no marge"
+    bot.send_message(chat_id=update.message.chat_id, text=text_str)
+
 class Argparser(object):
     def __init__(self):
         parser = argparse.ArgumentParser()
@@ -40,20 +54,28 @@ class Argparser(object):
         parser.add_argument("--dbg", action="store_true", help="debug", default=False)
         self.args = parser.parse_args()
 
+class Demon(Demon_Father):
+    def run(self):
+        updater = Updater(token=API_TOKEN)
+        dispatcher = updater.dispatcher
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+        start_handler = CommandHandler("start", start)
+        net_handler = CommandHandler("net", net)
+        high_handler = CommandHandler("high", high)
+        mg_handler = CommandHandler("mg", mg)
+        dispatcher.add_handler(start_handler)
+        dispatcher.add_handler(net_handler)
+        dispatcher.add_handler(high_handler)
+        dispatcher.add_handler(mg_handler)
+        updater.start_polling()
+
 # write code here
 def premain(argparser):
     signal.signal(signal.SIGINT, SigHandler_SIGINT)
     #here
-    updater = Updater(token=API_TOKEN)
-    dispatcher = updater.dispatcher
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    start_handler = CommandHandler("start", start)
-    net_handler = CommandHandler("net", net)
-    high_handler = CommandHandler("high", high)
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(net_handler)
-    dispatcher.add_handler(high_handler)
-    updater.start_polling()
+    pidfile = open("/tmp/telebot_temp", "w")
+    demon = Demon(pidfile)
+    demon.run()
 
 def main():
     argparser = Argparser()
