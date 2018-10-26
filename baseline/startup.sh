98c3a414201f5d697ec53b893c996837ffd34ee8
bloodstalker <thabogre@gmail.com>
2018-10-26 21:17:50 +0330

    fix

--- baseline/startup.sh
+++ baseline/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/abbatoir" ]; then
-  mkdir abbatoir
+  mkdir $HOME/abbatoir
 fi
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
