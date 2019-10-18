5af96ac25e296b57ae4778c491b86c2badf55296
bloodstalker <thabogre@gmail.com>
2019-10-18 17:55:11 +0330

    update

--- llvm-update.sh
+++ llvm-update.sh
@@ -6,6 +6,6 @@ svn up -r$REVISION
 (cd ./projects/compiler-rt; svn up -r$REVISION)
 (cd ./projects/libcxx; svn up -r$REVISION)
 (cd ./projects/libcxxabi; svn up -r$REVISION)
-(cd ./tools/lldb; svn up -r$REVISION)
+#(cd ./tools/lldb; svn up -r$REVISION)
 (cd ./tools/clang; svn up -r$REVISION)
 (cd ./tools/clang/tools/extra; svn up -r$REVISION)
