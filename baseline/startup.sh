3567e7d182bc1ef130ad8d801904f7db340ce464
bloodstalker <thabogre@gmail.com>
2020-03-22 11:15:42 +0430

    update

--- baseline/startup.sh
+++ baseline/startup.sh
@@ -6,9 +6,9 @@ cp .vimrc ~/
 if [ ! -d "$HOME/abbatoir" ]; then
   mkdir $HOME/abbatoir
 fi
-(cd ~/.oh-my-zsh/plugins; git clone https://github.com/zsh-users/zsh-syntax-highlighting.git)
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+(cd ~/.oh-my-zsh/plugins; git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins)
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins
 
