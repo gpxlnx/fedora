1bd8bb5fa7cfb45397b4e5451e94c75cd1796905
bloodstalker <thabogre@gmail.com>
2020-05-07 20:23:46 +0430

    update

--- baseline/startup.sh
+++ baseline/startup.sh
@@ -13,7 +13,6 @@ fi
 sudo $PCKMG install make cmake gcc gdb python3 vim lynx tmux zsh curl wget -y
 
 #copy dot files
-cp .zshrc ~/
 cp .tmux.conf ~/
 cp .vimrc ~/
 
@@ -28,15 +27,15 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 
+#oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp .zshrc ~/
 #autojump
 (cd ~; git clone https://github.com/wting/autojump)
 (cd ~/autojump; ./install.py)
 echo "#autojump" >> ~/.zshrc
 (cd ~; echo "[[ -s $(pwd)/.autojump/etc/profile.d/autojump.sh ]] && source $(pwd)/.autojump/etc/profile.d/autojump.sh" >> ~/.zshrc)
 echo "autoload -U compinit && compinit -u" >> ~/.zshrc
-
-#oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #zsh-syntax-highlighting
 (cd ~/.oh-my-zsh/plugins; git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting)
