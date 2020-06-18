b8b84a1fdc6462c0735d5c36efd416edcaed9698
bloodstalker <thabogre@gmail.com>
2020-06-18 11:54:21 +0430

    browser

--- lemonbar.sh
+++ lemonbar.sh
@@ -2,6 +2,10 @@
 #./lemonbar.sh| lemonbar -f "DejaVu Sans Mono for Powerline:size=7"
 
 NORMAL="%{F-}%{B-}"
+sep_left=""
+sep_right=""
+sep_l_left=""
+sep_l_right=""
 
 DATETIME() {
   DATETIME=$(date "+%a %b %d, %T")
@@ -19,6 +23,6 @@ MEM_SWAP() {
 }
 
 while true; do
-  echo -e "%{r}%{F#00ffff}%{B#ff0000}%{R} S:$(MEM_SWAP)%{F#ffff00}%{B#00ffff}\ue0b2%{R} M:$(MEM_RAM)%{F#0000ff}%{B#ffff00}\ue0b2%{R} $(DATETIME)$NORMAL"
+  echo -e "%{r}%{F#00ffff}%{B#ff0000}$sep_left%{R} S:$(MEM_SWAP)%{F#ffff00}%{B#00ffff}$sep_left%{R} M:$(MEM_RAM)%{F#0000ff}%{B#ffff00}$sep_left%{R} $(DATETIME)$NORMAL"
   sleep 1
 done
