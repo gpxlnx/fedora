8e2e403cb330926220b1d2e0e2cbf53c31397d28
bloodstalker <thabogre@gmail.com>
2020-08-08 18:28:39 +0430

    sourcing my zshrc is dogslow. the stripped version is very very fast.

--- bin/run_with_dmenu.sh
+++ bin/run_with_dmenu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 set -o pipefail
-source ~/.zshrc
+source ~/scripts/zshrc_stripped.sh
 COMMAND=$(echo "" | dmenu -p "Quick Term:")
 RESULT=$(eval "$COMMAND")
 echo $RESULT | dmenu -p "Quick Term Out:" > /dev/null
