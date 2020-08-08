264f207bce8c07a94f933cef36a066c56ab6ca54
bloodstalker <thabogre@gmail.com>
2020-08-08 17:27:41 +0430

    fixed the problem with the dmenu scripts. they work now.

--- bin/manga_dmenu.sh
+++ bin/manga_dmenu.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
 # until kaminokumo --manga; do done | dmenu -nhb "dark slate gray" -nhf white -shb "forest green" -shf white -l 12 -p "Manga List:" > /dev/null
+source ~/.zshrc
 until kaminokumo --manga; do done | dmenu -p "Manga List:" > /dev/null
 
--- bin/run_with_dmenu.sh
+++ bin/run_with_dmenu.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 set -o pipefail
+source ~/.zshrc
 COMMAND=$(echo "" | dmenu -p "Quick Term:")
 RESULT=$(eval "$COMMAND")
 echo $RESULT | dmenu -p "Quick Term Out:" > /dev/null
