8f25fbb0f77602a751f7020d6e8f017298790817
bloodstalker <thabogre@gmail.com>
2020-08-30 02:53:09 +0430

    update

--- tmux/vcs_info.sh
+++ tmux/vcs_info.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
 source common.sh
+SEPARATOR_RIGHT_BOLD=""
+SEPARATOR_RIGHT_THIN=""
+
+MIN_MAJOR_VERSION="2"
+MIN_MINOR_VERSION="1"
+TMUX_VERSION="$(tmux -V)"
+
+get_tmux_cwd() {
+  tmux display -p -F "#{pane_current_path}"
+}
+tmux_path=$(get_tmux_cwd)
+cd $tmux_path
+IFS='/' read -ra tmux_path_array <<< "$tmux_path"
+for i in "${tmux_path_array[@]}"
+do
+  shortened_path+=${i:0:1}/
+done
 
 function gitadditions {
   git rev-parse --git-dir > /dev/null 2>&1
@@ -18,9 +35,18 @@ function gitdeletions {
   fi
 }
 
-branch_info=$(git rev-parse --abbrev-ref HEAD)
-untracked_info=$(echo ⋯ $(git ls-files --others --exclude-standard | wc -w))
-deletions_info=$(gitdeletions)
-additions__info=$(gitadditions)
+function git_untracked_info {
+  git rev-parse --git-dir > /dev/null 2>&1
+  if [[ $? == 0 ]]; then
+    untracked=$(git ls-files --others --exclude-standard | wc -w)
+    echo ⋯ $untracked
+  fi
+}
 
-echo $(pwd)  $branch_info $SEPARATOR_RIGHT_BOLD $additions__info $SEPARATOR_RIGHT_BOLD $deletions_info $SEPARATOR_RIGHT_BOLD $untracked_info $SEPARATOR_RIGHT_BOLD
+tmux_session_info=" #S:#I.#P"
+branch_info="#[fg=colour255 bg=colour65]"$(git rev-parse --abbrev-ref HEAD)
+untracked_info="#[fg=colour255 bg=colour244]"$(echo ⋯ $(git ls-files --others --exclude-standard | wc -w))
+deletions_info="#[fg=colour255 bg=colour1]"$(gitdeletions)
+additions__info="#[fg=colour255 bg=colour22]"$(gitadditions)
+
+echo "#[fg=colour255 bg=colour61]$shortened_path #[fg=colour61 bg=colour26]$SEPARATOR_RIGHT_BOLD#[fg=colour255 bg=colour26]$tmux_session_info #[fg=colour26 bg=colour65]$SEPARATOR_RIGHT_BOLD $branch_info #[fg=colour65 bg=colour22]$SEPARATOR_RIGHT_BOLD $additions__info #[fg=colour22 bg=colour1]$SEPARATOR_RIGHT_BOLD $deletions_info #[fg=colour1 bg=colour244]$SEPARATOR_RIGHT_BOLD $untracked_info #[fg=colour244 bg=colour16]$SEPARATOR_RIGHT_BOLD"
